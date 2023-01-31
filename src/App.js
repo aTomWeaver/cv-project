23c5e6c17558bfa130da01618c08c2f14ac532b9
aTomWeaver <atomweavermail@gmail.com>
2023-01-31 06:25:24 -0500

    Education component render from state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Education from "./Components/EducationField";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <Education institution="example U"/>
+      </div>
+    );
   }
 }
 
--- src/Components/EducationField.js
+++ src/Components/EducationField.js
@@ -3,16 +3,44 @@ import { Component } from "react";
 class Education extends Component {
   constructor() {
     super();
+
+    this.state = {
+      cache: { name: "", degree: "", startYear: "", endYear: "" },
+      institutions: [
+        {
+          name: "University of Bongus, Jelly Campus",
+          degree: "B.A. in Wigging",
+          startYear: "2011",
+          endYear: "2015",
+        },
+        {
+          name: "University of Michigan",
+          degree: "B.A. in Scientology",
+          startYear: "2016",
+          endYear: "2019",
+        },
+      ],
+    };
   }
   render() {
+    const institutions = this.state.institutions;
+
     return (
-    <div>
-        <h3>{this.props.institution}h1</h3>
-        <p>{this.props.degree}first p</p>
-        <p>{this.props.startYear}-{this.props.endYear}second p</p>
-    </div>
+      <div>
+        {institutions.map((ins) => {
+          return (
+            <div key={ins.name}>
+              <h3>{ins.name}</h3>
+              <p>{ins.degree}</p>
+              <p>
+                {ins.startYear} - {ins.endYear}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
 
-export default Education
+export default Education;
